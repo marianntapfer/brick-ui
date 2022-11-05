246f95021dbb989ef4d099b1593e15686cd9617a
Mariann Tapfer <tapferm@gmail.com>
2022-11-05 14:25:17 +0200

    Publishing package, add css and optimizing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Brick UI
+
+Simple react components library. Collection of reusable Lego bricks to build up your UI. 
--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+
 import pkg from './package.json'
 
 
@@ -21,14 +26,18 @@ export default [
         },
       ],
       plugins: [
+        peerDepsExternal(),
         resolve(),
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
+        postcss(),
+        terser(),
       ],
     },
     {
       input: "dist/esm/types/index.d.ts",
       output: [{ file: "dist/index.d.ts", format: "esm" }],
       plugins: [dts()],
+      external: [/\.css$/], /**.css modules are external and should not be processed as part of our type definitions */
     },
   ];
