bf4cebb5d20e3c5afea84033a37aea7870d78487
Mariann Tapfer <tapferm@gmail.com>
2023-01-28 19:10:26 +0200

    test-runner

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
   //https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
   docs: {
@@ -20,6 +18,11 @@ const config: StorybookConfig = {
     // autodocs: true,
     autodocs: 'tag',
   },
-}
+  features: {
+    storyStoreV7: true,
+    interactionsDebugger:true,
+    buildStoriesJson:true
+  },
+};
 
-export default config
+export default config;
