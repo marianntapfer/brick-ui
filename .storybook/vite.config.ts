1acd4b321899248d7d5c57a325337761e33b609e
Mariann Tapfer <tapferm@gmail.com>
2024-12-05 21:24:27 +0200

    remove example story

--- .storybook/vite.config.ts
+++ .storybook/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
-// import react from '@vitejs/plugin-react';
 
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
