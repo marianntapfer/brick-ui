772337d031a3b703cd9cfa189f263550f6e29db1
Mariann Tapfer <tapferm@gmail.com>
2025-04-04 00:26:30 +0300

    make native carousel

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -1,17 +1,28 @@
+import React from 'react';
+
 import { Meta, StoryObj } from '@storybook/react';
 
 import { Carousel } from './Carousel';
-
-import React from 'react';
 
 const meta = {
   component: Carousel,
   tags: ['autodocs'],
   args: { children: 'Hello' },
+  parameters: {
+    backgrounds: {
+      values: [{ name: 'black', value: '#181818' }],
+    },
+  },
 } satisfies Meta<typeof Carousel>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  parameters: {
+    backgrounds: {
+      default: 'black',
+    },
+  },
+};
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import * as styles from './Carousel.css';
+
+// import * as styles from './Carousel.css';
+
+import './Carousel.css';
 
 export interface CarouselProps {
   className?: string;
@@ -7,8 +10,20 @@ export interface CarouselProps {
 }
 
 export const Carousel = ({ className, children }: CarouselProps) => {
-  const classes: string[] = [styles.main];
+  const classes: string[] = ['brick-ui-carousel'];
   className && classes.push(className);
 
-  return <div className={classes.join(' ')}>{children}</div>;
+  const images = [237, 239, 76, 57, 42, 56, 78];
+
+  return (
+    <ul className={classes.join(' ')}>
+      {images.map((img) => {
+        return (
+          <li>
+            <img src={`https://picsum.photos/id/${img}/300/300`} alt="" />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
