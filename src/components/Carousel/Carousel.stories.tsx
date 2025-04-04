cafbcdd50652a51bbb34e06ea8835523992c193e
Mariann Tapfer <tapferm@gmail.com>
2025-04-04 09:11:37 +0300

    add props and configure reusability

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -4,13 +4,18 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { Carousel } from './Carousel';
 
+const items: React.ReactNode[] = [237, 239, 76, 57, 42, 56, 78].map((img) => (
+  <img src={`https://picsum.photos/id/${img}/300/400`} alt="" />
+));
+
 const meta = {
   component: Carousel,
   tags: ['autodocs'],
-  args: { children: 'Hello' },
+  args: { children: items },
   parameters: {
     backgrounds: {
       values: [{ name: 'black', value: '#181818' }],
+      default: 'black',
     },
   },
 } satisfies Meta<typeof Carousel>;
@@ -19,10 +24,15 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  parameters: {
-    backgrounds: {
-      default: 'black',
-    },
+export const Default: Story = {};
+
+export const Width: Story = {
+  args: {
+    width: '400px',
+  },
+};
+export const Shadow: Story = {
+  args: {
+    showShadow: true,
   },
 };
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // import * as styles from './Carousel.css';
 
@@ -6,24 +6,34 @@ import './Carousel.css';
 
 export interface CarouselProps {
   className?: string;
-  children: React.ReactNode;
+  children: React.ReactNode[];
+  width?: string;
+  showShadow?: boolean;
 }
 
-export const Carousel = ({ className, children }: CarouselProps) => {
+export const Carousel = ({
+  className,
+  children,
+  width,
+  showShadow,
+}: CarouselProps) => {
   const classes: string[] = ['brick-ui-carousel'];
   className && classes.push(className);
+  showShadow && classes.push('brick-ui-carousel__shadow');
 
-  const images = [237, 239, 76, 57, 42, 56, 78];
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (listRef.current && width) {
+      listRef.current.style.setProperty('--carousel-width', width);
+    }
+  }, []);
 
   return (
-    <ul className={classes.join(' ')}>
-      {images.map((img) => {
-        return (
-          <li>
-            <img src={`https://picsum.photos/id/${img}/300/300`} alt="" />
-          </li>
-        );
-      })}
+    <ul className={classes.join(' ')} ref={listRef}>
+      {children.map((child) => (
+        <li className="brick-ui-carousel-item">{child}</li>
+      ))}
     </ul>
   );
 };
